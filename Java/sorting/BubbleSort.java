d6bb30d58b21ad9e84babdbc5d3b379ff9415408
asharavesh <124661201+asharavesh@users.noreply.github.com>
2023-10-12 22:37:31 +0530

    Update BubbleSort.java

--- Java/sorting/BubbleSort.java
+++ Java/sorting/BubbleSort.java
@@ -1,36 +1,27 @@
-// Java program for implementation of Bubble Sort 
-class BubbleSort 
-{ 
-    void bubbleSort(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i = 0; i < n-1; i++) 
-            for (int j = 0; j < n-i-1; j++) 
-                if (arr[j] > arr[j+1]) 
-                { 
-                    // swap arr[j+1] and arr[j] 
-                    int temp = arr[j]; 
-                    arr[j] = arr[j+1]; 
-                    arr[j+1] = temp; 
-                } 
-    } 
-  
-    /* Prints the array */
-    void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i] + " "); 
-        System.out.println(); 
-    } 
-  
-    // Driver method to test above 
-    public static void main(String args[]) 
-    { 
-        BubbleSort ob = new BubbleSort(); 
-        int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
-        ob.bubbleSort(arr); 
-        System.out.println("Sorted array"); 
-        ob.printArray(arr); 
-    } 
-} 
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 56, 67, , 34, 65, 98};
+        bubbleSort(arr);
+        printArray(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    //swapping
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+//print the sorted array
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
+
