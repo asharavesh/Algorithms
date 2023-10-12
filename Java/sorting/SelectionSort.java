381a39ccc47077fbbf2aa689789ca25256ff0428
asharavesh <124661201+asharavesh@users.noreply.github.com>
2023-10-12 22:42:11 +0530

    Update SelectionSort.java

--- Java/sorting/SelectionSort.java
+++ Java/sorting/SelectionSort.java
@@ -1,44 +1,30 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] array = {10, 67, 28, 31, 20, 78, 34};
+        selectionSort(array);
+        printArray(array);
+    }
 
-// Java program for implementation of Selection Sort 
-class SelectionSort 
-{ 
-    void sort(int arr[]) 
-    { 
-        int n = arr.length; 
-  
-        // One by one move boundary of unsorted subarray 
-        for (int i = 0; i < n-1; i++) 
-        { 
-            // Find the minimum element in unsorted array 
-            int min_idx = i; 
-            for (int j = i+1; j < n; j++) 
-                if (arr[j] < arr[min_idx]) 
-                    min_idx = j; 
-  
-            // Swap the found minimum element with the first 
-            // element 
-            int temp = arr[min_idx]; 
-            arr[min_idx] = arr[i]; 
-            arr[i] = temp; 
-        } 
-    } 
-  
-    // Prints the array 
-    void printArray(int arr[]) 
-    { 
-        int n = arr.length; 
-        for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
-        System.out.println(); 
-    } 
-  
-    // Driver code to test above 
-    public static void main(String args[]) 
-    { 
-        SelectionSort ob = new SelectionSort(); 
-        int arr[] = {64,25,12,22,11}; 
-        ob.sort(arr); 
-        System.out.println("Sorted array"); 
-        ob.printArray(arr); 
-    } 
-} 
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    //printed the sorted array
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
